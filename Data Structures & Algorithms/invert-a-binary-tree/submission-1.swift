77473770618260869edfe74ffdcea014d2f97fa8
Okikiola <okikday@gmail.com>
2026-08-28 20:23:09 +0100

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.swift
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     var val: Int
+ *     var left: TreeNode?
+ *     var right: TreeNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        if root?.left == nil && root?.right == nil{
+            return root
+        }else{
+            let tmp = root?.left
+            root?.left = root?.right
+            root?.right = tmp
+            invertTree(root?.left)
+            invertTree(root?.right)
+        }
+        return root
+    }
+}
