c85b76711883c39a053bd51e4fa9fc18ad50c0eb
Okikiola <okikday@gmail.com>
2026-09-07 00:53:15 +0100

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        total = 0
+        nums = [0] * 32
+        for i in range(32):
+            total += (n & 1) * 2**(31-i)
+            n = n >> 1
+        return total
